b4d2b00db57105a5cd7d4480ad9d7d2b427cbbaa
Yagna123k <yagna82kb@gmail.com>
2024-02-20 11:27:51 +0530

    Fetched data from the server

--- client/src/components/MovieCards/M_MovieCard.jsx
+++ client/src/components/MovieCards/M_MovieCard.jsx
@@ -4,59 +4,9 @@ import { BsFillStarFill, BsHandThumbsDown, BsHandThumbsUp, BsPlayCircleFill, BsP
 import {AddIcon} from '@chakra-ui/icons'
 
 const M_MovieCard = ({Movie_Data}) => {
-    // const Movie_Data = {
-    //     Title: "Baahubali: The Beginning",
-    //     "Rating": 4.9,
-    //     "TrailerURL": "https://www.youtube.com/watch?v=sOEg_YZQsTI",
-    //     "CoverIMG": "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/8662/1368662-i-bd252b4dba15",
-    //     "Moods": ["Epic", "Adventure", "Action"],
-    //     "Platform":"https://www.hotstar.com/in/movies/baahubali-the-beginning/1000074338/watch?filters=content_type%3Dmovie",
-    //     "Likes":10,
-    //     "Dislikes":3
-    //     }
+
   return (
     <div>
-        {/* <Box position="relative" w="40%" h="300" borderRadius="md" bg="#00050D" cursor="pointer" justifyContent="right" display="flex">
-            <Image src={Movie_Data.CoverIMG} alt="Card Image" maxW="auto" h="300" borderRadius="md" />
-            <Box position="absolute" left="4" top="4">
-                <Flex direction='column' alignItems="center">
-                    <Button><BsHandThumbsUp/></Button>
-                    <Text>{Movie_Data.Likes}</Text>
-                    <Button><BsHandThumbsDown/></Button>
-                    <Text>{Movie_Data.Dislikes}</Text>
-                    <Button><AddIcon/></Button>
-                </Flex>
-            </Box>
-            <div
-                style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    background: 'linear-gradient(to right, rgba(0, 5, 13, 1.5), transparent)',
-                }}
-            />
-            <Box position={'absolute'}  bottom={10}>
-                <Text>{Movie_Data.Title}</Text>
-                <HStack>
-                    <Text>Rating:<BsFillStarFill />{Movie_Data.Rating}</Text>
-                </HStack>
-                <HStack>
-                    {Movie_Data.Moods.map((mood, id)=>{
-                        if (id==Movie_Data.Moods.length-1){
-                            return <Text key={id}>{mood}</Text>
-                        }
-                        return <Text key={id}>{mood} | </Text>
-                    })}
-                </HStack>
-                <HStack>
-                    <Button leftIcon={<BsPlayFill />}>Watch Now</Button>
-                    <Button><AddIcon/></Button>
-                </HStack>
-            </Box>
-            
-        </Box>*/}
         <Card maxW='sm'>
         <Box bg="#00050D" p={4} borderRadius="md" border="2px solid #33373d">
                 <Stack spacing={4}>
--- client/src/components/MovieCards/Movies_Page.jsx
+++ client/src/components/MovieCards/Movies_Page.jsx
@@ -8,7 +8,7 @@ function Movies_Page({lang}) {
     useEffect(()=>{
         const fetchMovies = async () => {
             try {
-              const response = await fetch('http://localhost:3000/movies');
+              const response = await fetch('https://cinemood-b811.onrender.com/movies');
               if (!response.ok) {
                 throw new Error('Failed to fetch movies');
               }
